d93b481d29c2dcd9dd4db17af8cd79481c3aa51c
Isak sayyed <isaksayyed0@gmail.com>
2021-12-31 18:13:27 +0530

    created Footer & NewsLetter

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import NavBar from '../components/NavBar'
 import Announcement from '../components/Announcement'
 import Slider from '../components/Slider'
+import Categories from '../components/Categories'
+import Products from '../components/Products'
+import NewsLetter from '../components/NewsLetter'
+import Footer from '../components/Footer.jsx'
+
+
 const Home =() =>{
   return (
     <div>
     <Announcement/>
     <NavBar/>
     <Slider/>
+    <Categories/>
+    <Products />
+    <NewsLetter/>
+    <Footer/>
     </div>
   )
 }
